f0731df7e2e75e9bf8cb2029ba15552bfd8f58a5
priteshp97 <88158414+priteshp97@users.noreply.github.com>
2021-11-26 13:15:24 +0000

    persist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import './App.css';
-import Footer from './components/Footer';
-import MyNavBar from './components/MyNavBar';
-import { BrowserRouter as Router} from 'react-router-dom'
-import { useState, useEffect } from "react"
+import "./App.css";
+import Footer from "./components/Footer";
+import MyNavBar from "./components/MyNavBar";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState, useEffect } from "react";
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import thunk from "redux-thunk";
+import songsReducer from "./redux/reducers/songs";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
 
+const mainCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const initialState = {
+  data: {
+    songs: [],
+    albums: [],
+    artist: [],
+  },
+  liked: [],
+};
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const allReducers = combineReducers({
+  data: songsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
+const store = createStore(
+  persistedReducer,
+  initialState,
+  mainCompose(applyMiddleware(thunk))
+);
+
+let persistor = persistStore(store);
 
 function App() {
-
-  const [artist, setArtist] = useState(null)
+  const [artist, setArtist] = useState(null);
 
   return (
     <div className="App">
       <Router>
-      <MyNavBar artist={artist} setArtist={setArtist} />
-      <Footer />
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <MyNavBar artist={artist} setArtist={setArtist} />
+            <Footer />
+          </PersistGate>
+        </Provider>
       </Router>
     </div>
   );
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,30 +1,43 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
-import thunk from "redux-thunk"
+import thunk from "redux-thunk";
 import likedReducer from "../reducers/liked";
 import songsReducer from "../reducers/songs";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
 
-
-
-const mainCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const mainCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const initialState = {
-    data: {
-        songs: [],
-        albums: [],
-        artist: [],
-        isLoading: true
-    },
-    likes: {
-        liked: []
-    }
-}
+  data: {
+    songs: [],
+    albums: [],
+    artist: [],
+    isLoading: true,
+  },
+  likes: {
+    liked: [],
+  },
+};
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const allReducers = combineReducers({
-    data : songsReducer,
-    likes: likedReducer
-})
+  data: songsReducer,
+  likes: likedReducer,
+});
 
-const store = createStore(allReducers, initialState, mainCompose(applyMiddleware(thunk)))
+const persistedReducer = persistReducer(persistConfig, allReducers);
 
-export default store
+const store = createStore(
+  persistedReducer,
+  initialState,
+  mainCompose(applyMiddleware(thunk))
+);
 
+let persistor = persistStore(store);
+
+export default store;
